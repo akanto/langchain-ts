c12cf9131aa968b5d7cf97d79ba67c03d00b23c5
Attila Kanto <attila.kanto@gmail.com>
2024-01-12 15:33:06 +0100

    Validate that TAVILY_API_KEY is present

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,9 +1,13 @@
-import { logger } from "./logging";
+import { logger } from './logging';
 
 export function validate(): void {
-    logger.info("Validating environment variables");
-    if (!process.env.OPENAI_API_KEY) {
-        logger.error("OPENAI_API_KEY environment variable is not present");
-        process.exit(1);
-    }
+  logger.info('Validating environment variables');
+  if (!process.env.OPENAI_API_KEY) {
+    logger.error('OPENAI_API_KEY environment variable is not present');
+    process.exit(1);
+  }
+  if (!process.env.TAVILY_API_KEY) {
+    logger.error('TAVILY_API_KEY environment variable is not present');
+    process.exit(1);
+  }
 }
