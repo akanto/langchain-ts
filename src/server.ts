5f65cc67548fd9f8b811145001c661c833e69c92
Attila Kanto <attila.kanto@gmail.com>
2024-01-14 13:21:36 +0100

    Print out node version

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { morganMiddleware, logger } from './utils/logging';
 import langchainRoutes from './routes/langchainRoutes';
 import { validate } from './utils/validate';
 
+logger.info('Node version: %s', process.version);
+
 validate();
 
 const app = express();
@@ -13,10 +15,8 @@ app.use(morganMiddleware);
 // Enable CORS
 app.use(cors());
 
-
 app.use('/api/langchain', langchainRoutes);
 
-
 app.listen(PORT, () => {
-    logger.info(`Server is running on http://localhost:${PORT}`);
+  logger.info(`Server is running on http://localhost:${PORT}`);
 });
